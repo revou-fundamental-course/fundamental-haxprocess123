6b917b8c4d06df430d3fce5a4ed4eedc6a05a6df
Your Name <you@example.com>
2024-07-04 17:01:58 +0700

    mini project revou

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
-// Ini Javascript
+function validateForm() {
+  const name = document.forms["message-form"]["full-name"].value;
+  const gender = document.forms["message-form"]["gender"].value;
+  const bookdate = document.forms["message-form"]["book-date"].value;
+  const notes = document.forms["message-form"]["notes"].value;
 
+
+  if (name == "" || gender == "" || bookdate == "" || notes == "") {
+      alert("Please Complete The Form");
+      return false;
+
+  }
+
+  setSenderUI(name, gender, bookdate, notes);
+
+  return false;
+
+
+}
+
+function setSenderUI (name, gender, bookdate, notes) {
+
+  document.getElementById("sender-full-name").innerHTML = name;
+  document.getElementById("sender-gender").innerHTML = gender;
+  document.getElementById("sender-book-date").innerHTML = bookdate;
+  document.getElementById("sender-notes").innerHTML = notes;
+  
+
+
+}
